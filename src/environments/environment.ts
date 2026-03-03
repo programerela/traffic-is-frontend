1397ab02d150c93348a59aa079b27279e04b6d91
Ela Curic <curicela@gmail.com>
2026-03-03 17:02:02 +0100

    backendUrl fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://traffic-is-back.onrender.com',
+  apiUrl: 'https://traffic-is-back.onrender.com/api',
   useMockData: false  // ← PROMENI NA false kada pokreneš backend
 };
 
 // src/environments/environment.prod.ts
 export const environmentProd = {
   production: true,
-  apiUrl: 'https://your-production-url.com/api',
+  apiUrl: 'https://traffic-is-back.onrender.com/api',
   useMockData: false
 };
