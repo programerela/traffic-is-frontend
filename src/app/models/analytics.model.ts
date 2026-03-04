6aa930067c5978c9463423d944b5a1d6f96f9933
Ela Curic <curicela@gmail.com>
2026-03-04 15:19:34 +0100

    incidentiPoStatusu

--- src/app/models/analytics.model.ts
+++ src/app/models/analytics.model.ts
@@ -37,7 +37,7 @@ export interface PeriodIzvestaj {
   period: string;
   ukupnoIncidenata: number;
   incidentiPoTezini: { [key: string]: number };
-  incidentiPoStatusu: { [key: string]: number };
+  incidentiPoStatusu?: { [key: string]: number };
   ukupnoKazni: number;
   ukupanIznosKazni: number;
   ukupnoPovređenih: number;
